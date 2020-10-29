17032774b4207aac6d6b845394082daae6b38a3f
Shreyansh <sshreyansh199@gmail.com>
2020-10-29 22:05:16 +0530

    scroll to top added

---
+++ script/scrollButton.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    
+    var body = document.body,
+    html = document.documentElement;
+
+    var height = Math.max( body.scrollHeight, body.offsetHeight, 
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > window.innerHeight) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+    
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+    
+});
